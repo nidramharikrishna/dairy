6a6436f4edf85c9165c971efd93aeec8c2f265ba
HariKrishna <nidramharikrishna@email.com>
2026-06-18 14:43:30 +0530

    Fix admin role response

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,21 +1,20 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data.get("email"),
+            email=validated_data.get("email", ""),
             password=validated_data["password"],
         )
-        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
